70eb34f0d334df397f9a15edb0ccd9131d9f2658
Camilo <camian6@outlook.com>
2024-02-22 07:05:29 -0500

    Some components and views were updated
    The home view was chaged to add into the main page the app, also
    componets by default were deleted.

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome vue</h1>
   </main>
 </template>
