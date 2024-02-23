1da39bdd19bd490c869c0b0404477b34e6af3ed3
Camilo <camian6@outlook.com>
2024-02-22 19:35:11 -0500

    Main component was modified
    The app component was modified thier styles, was added the navbar
    component and a footer to main app

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,10 +1,41 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HomeView from "./views/HomeView.vue";
+import { RouterView } from "vue-router";
+import NavBar from "./components/navBar/Navbar.vue";
 </script>
 
 <template>
-  <RouterView />
+  <NavBar />
+  <main class="container mx-auto py-2">
+    <RouterView />
+  </main>
+
+  <footer>
+    <span class="content">Todos los derechos reservados &copy;</span>
+  </footer>
 </template>
 
-<style scoped></style>
+<style>
+body {
+  background-color: rgb(231 231 231);
+  /* display: flex; */
+}
+
+main {
+  min-height: 100vh;
+}
+
+footer {
+  color: white;
+  background-color: #1f2937;
+  height: 60px;
+  margin-top: auto;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  text-align: center;
+}
+
+.content {
+  text-align: end;
+  padding: 10px;
+}
+</style>
