69b63797481f399a5fec324daadc90e648851c57
Camilo <camian6@outlook.com>
2024-02-22 22:04:05 -0500

    home component was added

---
+++ front-end/src/components/Home/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Welcome vue</h1>
+
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<script>
+export default {
+  name: "HomeComponent",
+};
+</script>
