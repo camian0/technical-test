a8e9707f700e959d1ef8fde614f43efc6ad5d977
Camilo <camian6@outlook.com>
2024-02-22 19:38:08 -0500

    Navbar component was added and modified index page
    the nav bar component was created, also their information for navegate
    into pages was added, also the styles were added to customize it

---
+++ front-end/src/components/navBar/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="bg-gray-800">
+    <div class="content">
+      <RouterLink to="/">Inicio</RouterLink>
+      <RouterLink to="/search">Buscar</RouterLink>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+</script>
+
+<style scoped>
+nav {
+  height: 55px;
+  color: white;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+.content {
+  /* background-color: blueviolet; */
+  text-align: right;
+  width: 100%;
+  padding: 15px;
+}
+
+a {
+  margin: 10px;
+  padding: 10px;
+}
+
+a:hover {
+  background-color: #374151;
+  padding: 15px 10px;
+  border-radius: 10px;
+}
+
+.router-link-active {
+  background-color: #4b5563;
+  padding: 15px 10px;
+  border-radius: 10px;
+}
+</style>
