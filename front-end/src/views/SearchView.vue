7cc7e7088abf9fabc96cbf2baa6c14c8d7aab6a5
Camilo <camian6@outlook.com>
2024-02-28 11:13:47 -0500

    The Search component was added
    Search component was added into search view

--- front-end/src/views/SearchView.vue
+++ front-end/src/views/SearchView.vue
@@ -1,12 +1,17 @@
 <template>
   <h1>Bienvenido a la página de buscar</h1>
+  <SearchComponent />
 </template>
 
 <script>
+import SearchComponent from "../components/Search/SearchComponent.vue";
 export default {
   name: "SearchView",
   data() {
     return {};
+  },
+  components: {
+    SearchComponent,
   },
 };
 </script>
