4bd9a0190a737058fb336461407183733c48edfd
Camilo <camian6@outlook.com>
2024-02-28 23:59:04 -0500

    The table and logic was added but is not finished
    The table with their styles were added, also the logic to do a request
    and process the data that come back

--- front-end/src/components/Search/SearchComponent.vue
+++ front-end/src/components/Search/SearchComponent.vue
@@ -11,48 +11,109 @@
     <button
       type="submit"
       class="rounded-md bg-blue-400 mx-2 my-2.5 px-6 py-4 text-sm right-0 font-semibold text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      v-on:click="buscar"
+      v-on:click="search"
     >
       Buscar
     </button>
   </div>
-  {{ model.search }}
+
+  <section
+    class="card relative isolate overflow-hidden px-6 py-24 sm:py-32 lg:px-8 rounded-md mx-2 my-2"
+    v-if="tableData.length == 0"
+  >
+    <div class="mx-auto max-w-2xl lg:max-w-4xl">
+      <p class="flex justify-center">
+        La tabla no contiene información para mostrar
+      </p>
+    </div>
+  </section>
+
+  <div class="table mx-2 my-5" v-if="tableData.length != 0">
+    <table class="table-fixed border-collapse">
+      <thead>
+        <tr>
+          <th class="">Fecha</th>
+          <th class="">Desde</th>
+          <th class="">Para</th>
+          <th class="">Asunto</th>
+          <th class="">Contenido</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="even:bg-gray-50 odd:bg-gray-300" v-for="item in tableData">
+          <td style="min-width: 205px" class="">{{ item.Date }}</td>
+          <td class="">{{ item.From }}</td>
+          <td class="">{{ item.To }}</td>
+          <td style="min-width: 215px" class="">{{ item.Subject }}</td>
+          <td class="">
+            <pre>
+            {{ item.Content }}
+            </pre>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+import { postData } from "../../Request/Request";
 export default {
   name: "SearchComponent",
-
   data() {
     return {
+      data: [],
+      tableData: [],
+      body: {
+        search_type: "match",
+        query: {
+          term: "",
+          field: "",
+        },
+        sort_fields: ["-@timestamp"],
+        from: 0,
+        max_results: 20,
+        _source: [],
+      },
       model: {
         search: "",
       },
-      people: [
-        {
-          name: "Leslie Alexander",
-          role: "Co-Founder / CEO",
-          imageUrl:
-            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-        {
-          name: "Juan ",
-          role: "Co-Founder / CEO",
-          imageUrl:
-            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-        {
-          name: "Pepito",
-          role: "Co-Founder / CEO",
-          imageUrl:
-            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-      ],
+      indexedDB: "enron",
     };
   },
   methods: {
-    buscar() {
-      console.log("dio click en el boton");
+    async search() {
+      this.body.query.field = "content";
+      this.body.query.term = "fuck";
+      await postData(`api/${this.indexedDB}/_search`, this.body)
+        .then((res) => {
+          if (res != null) {
+            this.data = res;
+            console.log("data recibida", this.data);
+          }
+
+          this.processData(this.data.hits.hits);
+        })
+        .catch((err) => {
+          console.log("error", err);
+        });
+    },
+
+    processData(emails) {
+      let processed = [];
+      emails.forEach((item) => {
+        let object = {
+          Date: item._source.Date,
+          From: item._source.From,
+          To: item._source.To,
+          Subject: item._source.Subject,
+          Content: item._source.content,
+        };
+
+        processed.push(object);
+      });
+
+      this.tableData = processed;
     },
   },
 };
@@ -67,5 +128,23 @@ export default {
 
 .search input {
   min-width: 90%;
+}
+
+table {
+  background-color: white;
+  border-radius: 5px;
+  overflow-y: visible;
+}
+
+th {
+  padding: 5px;
+}
+
+td {
+  padding: 3px;
+}
+
+.card {
+  background-color: rgb(255 255 255 / 16%);
 }
 </style>
