067e51defcb0e227509e4de45a67ba0d3cdecab0
Camilo <camian6@outlook.com>
2024-03-01 12:08:05 -0500

    Route to get information from zincsearch was added
    the route was finished for get the data from zincsearch

--- back-end/main/Routes.go
+++ back-end/main/Routes.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"io/ioutil"
+	"myapp.com/enron/helpers"
+	"myapp.com/enron/model"
 	"net/http"
 )
+
+const ZINCURL = "http://localhost:4080/api/enron/_search"
 
 func Routes() *chi.Mux {
 	mux := chi.NewMux()
@@ -15,6 +22,7 @@ func Routes() *chi.Mux {
 		middleware.Recoverer,
 	)
 	mux.Get("/", helloHandler)
+	mux.Post("/search", searchZicHandler)
 	return mux
 }
 
@@ -23,4 +31,47 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 	res := map[string]interface{}{"message": "hello"}
 	_ = json.NewEncoder(w).Encode(res)
+}
+
+func searchZicHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
+	userEncoded := helpers.EncodeUser()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(res, "Error al leer el cuerpo de la solicitud", http.StatusInternalServerError)
+		return
+	}
+	var jsonStr = []byte(string(body))
+	reqPost, err := http.NewRequest("POST", ZINCURL, bytes.NewBuffer(jsonStr))
+	reqPost.Header.Set("Content-Type", "application/json")
+	reqPost.Header.Set("Authorization", "Basic "+userEncoded)
+	if err != nil {
+		http.Error(res, "Error al leer el cuerpo de la solicitud", http.StatusInternalServerError)
+		return
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(reqPost)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		http.Error(res, "Error al leer el cuerpo de la solicitud", http.StatusInternalServerError)
+		return
+	}
+
+	var decode model.Response
+	err = json.Unmarshal(body, &decode)
+	if err != nil {
+		fmt.Println("Error al decodificar JSON:", err)
+		return
+	}
+
+	decodeData := map[string]interface{}{"data": decode}
+
+	defer response.Body.Close()
+	_ = json.NewEncoder(res).Encode(decodeData)
 }
