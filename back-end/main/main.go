ec95c5698680b0922fa443f15059c2b6fba8b98c
Camilo <camian6@outlook.com>
2024-03-19 18:39:49 -0500

    The methods were called
    The method were called into main file, also the code to generate de cpu
    profiling into mbox convert, their file was generated with few data

--- back-end/main/main.go
+++ back-end/main/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"myapp.com/enron/mbox"
 	server2 "myapp.com/enron/server"
 )
 
 func main() {
+	mbox.ConvertirMbox()
+	mbox.ReadMbox()
+
 	mux := Routes()
 	server := server2.CreateServer(mux)
 	fmt.Println("Servidor corriendo...")
